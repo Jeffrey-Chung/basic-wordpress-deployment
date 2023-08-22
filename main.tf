d5cf75964e6688cc506625ddb87f310fea1bf460
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-08-22 16:56:01 +1000

    Only create key pair if it's not created yet

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "wordpress-security-group" {
     ingress {
         cidr_blocks = [ "0.0.0.0/0" ]
         description = "allow http"
-        from_port = 0
+        from_port = 80
         to_port = 80
         protocol = "tcp"
         }
