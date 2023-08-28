9c39540c604da1d6cefcc98ea5842780d983bfed
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-08-28 11:46:24 +1000

    Add -y for installation

--- main.tf
+++ main.tf
@@ -44,14 +44,14 @@ resource "aws_instance" "wordpress-instance" {
   provisioner "remote-exec" {
     inline = [
       "sudo yum update -y",
-      "sudo yum install git",
+      "sudo yum install -y git",
       "sudo amazon-linux-extras install docker",
-      "sudo yum install docker",
+      "sudo yum install -y docker",
       "sudo service docker start",
       "sudo usermod -a -G docker ec2-user",
       "sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
-      "docker-compose version",
+      "sudo docker-compose version",
       "git clone https://github.com/Jeffrey-Chung/basic-wordpress-deployment",
       "echo $RANDOM > db_password.txt",
       "sudo docker-compose up -d"
